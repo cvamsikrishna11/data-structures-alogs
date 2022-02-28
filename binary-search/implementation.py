25d63b7cc4788f374e0de3d0e1577e8946656cc0
vamsichunduru <cvamsikrishna11@gmail.com>
2022-02-28 19:47:52 +0000

    Binary search

--- binary-search/implementation.py
+++ binary-search/implementation.py
@@ -1,24 +1,28 @@
+"""
+Binary search
+""" 
 arr = [1,2,3,4,5,6]
-goal = 6
+
+goal = 4
 
 
 def binarySearch(array, target):
-  left = 0
-  right = len(array)-1
-  while(left<=right):
-    mid=(left+right)//2 # 0+5 //2 --> 2
+  start = 0
+  end = len(array)-1
+  while(start <=end):
+    mid=(start+end)//2
     if(array[mid]==target):
       return mid   
     elif array[mid]<target:
-      left = mid + 1
+      start = mid + 1
     else:
-      right = mid-1
+      end = mid-1
   return -1        
 
 
-result = binarySearch(arr, goal) # index: 5
+result = binarySearch(arr, goal)
 
 if result !=-1:
-  print('Element present at index: {}'.format(arr[result]))
+  print('Element present at index: {}'.format(result))
 else: 
   print('Element not present in the array!')
